2dd0e62fb2c2b0aa82b70e2bac071f1b3d0dce91
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-04 02:15:05 -0500

    Case methods and the reverse method

---
+++ 2_Strings_I/Case_Methods.rb
@@ -0,0 +1,20 @@
+puts "hello".capitalize 
+# Hello
+puts "heLLo".capitalize
+# Hello
+puts "Hello".capitalize
+# Hello
+puts "hello World".capitalize
+# Hello world
+
+puts "John123".upcase
+# JOHN123
+puts "blah blah blah".upcase
+# BLAH BLAH BLAH
+
+puts "B0RIS".downcase 
+# b0ris
+
+puts "boRIS".downcase
+# boris
+
---
+++ 2_Strings_I/The_reverse_Method.rb
@@ -0,0 +1,5 @@
+puts "Ruby".reverse
+# ybuR
+
+# you can chaining methods one after another
+puts "Ruby is fun".upcase.reverse.downcase
