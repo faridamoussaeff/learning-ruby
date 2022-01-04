5d990b864ed4835b2961b5e664f49faad6bb3161
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-04 02:49:41 -0500

    Local Variables

---
+++ 3_Methods_and_Conditionals/Local_Variables.rb
@@ -0,0 +1,9 @@
+def introduce_myself
+    expression = "I am a genius"
+    puts expression
+end
+
+introduce_myself
+# I am a genius
+puts expression 
+# undefined local variable or method `expression'. Variable expression is considered to be local, like in JS it is block scoped
