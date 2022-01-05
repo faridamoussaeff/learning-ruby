77e1b78f1f8278ae138a752f8527121d0b00b863
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-04 21:37:09 -0500

    Truthiness and Falsiness

---
+++ 3_Methods_and_Conditionals/Truthiness_and_Falsiness.rb
@@ -0,0 +1,22 @@
+if true 
+    puts "That is true"
+end   #=> That is true
+
+if 5
+  puts "That is true"
+end    #=> That is true
+
+if []
+  puts "That is true"
+end    #=> That is true
+
+
+# Besides false and nill, everything in Ruby is truthy/true 
+if nil 
+  puts "That's nil"
+end 
+
+if false
+  puts "That's false"
+end
+
