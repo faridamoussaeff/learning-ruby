d0c7ad41b5cd25fbcc4c948f3b6ad916cf44eaab
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-04 22:24:38 -0500

    Elsif statement

---
+++ 3_Methods_and_Conditionals/Else_If.rb
@@ -0,0 +1,18 @@
+color = "Green"
+if color == "Red"
+  puts "Red is rad"
+elsif color == "Green"
+  puts "Great is great!"
+elsif color == "yellow"
+  puts "yah"
+end
+
+number = 25
+
+if number < 25
+  puts "That is a low number"
+elsif number < 50
+  puts "that is a number in the middle "
+elsif number < 73
+  puts "that is a big number"
+end  
